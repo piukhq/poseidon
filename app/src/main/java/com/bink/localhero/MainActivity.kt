ea982878fe3bcaf8270115e802c5aedf7423bb98
Enoch Hankombo <ehankombo@bink.com>
2022-02-21 16:34:12 +0000

    Added cpp and keys object

--- app/src/main/java/com/bink/localhero/MainActivity.kt
+++ app/src/main/java/com/bink/localhero/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bink.localhero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bink.localhero.utils.Keys
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Main",Keys.testApiKey())
     }
 }
