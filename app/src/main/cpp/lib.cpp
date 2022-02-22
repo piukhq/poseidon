d175cc46de8dc6ec2261e35e28eb89ff0b7f45e3
Enoch Hankombo <ehankombo@bink.com>
2022-02-22 15:26:20 +0000

    Added lib.cpp

--- app/src/main/cpp/lib.cpp
+++ app/src/main/cpp/lib.cpp
@@ -5,7 +5,7 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_bink_localhero_utils_Keys_testApiKey(
         JNIEnv *env,
         jobject) {
-    std::string key = "TestApiKey";
+    std::string key = "TestApiKey123";
     return env->NewStringUTF(key.c_str());
 }
 
