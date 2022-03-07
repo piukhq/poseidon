8b104a07244f9db44a43be55c3ff9336f77be6fd
Enoch Hankombo <ehankombo@bink.com>
2022-03-07 18:52:48 +0000

    List showing

--- app/src/main/java/com/bink/localhero/screens/wallet/WalletFragment.kt
+++ app/src/main/java/com/bink/localhero/screens/wallet/WalletFragment.kt
@@ -6,29 +6,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bink.localhero.R
+import com.bink.localhero.databinding.WalletFragmentBinding
 import com.bink.localhero.model.loyalty_plan.LoyaltyPlan
+import com.bink.localhero.screens.wallet.adapter.WalletAdapter
 import com.bink.localhero.utils.ui_state.WalletUiState
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class WalletFragment : Fragment() {
 
-    private val viewModel : WalletViewModel by viewModel()
+    private val viewModel: WalletViewModel by viewModel()
+    private var _binding: WalletFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var walletAdapter: WalletAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.wallet_fragment, container, false)
+         _binding = WalletFragmentBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        walletAdapter = WalletAdapter(mutableListOf())
+        setupRecyclerView()
         viewModel.getPlans()
 
-        viewModel.walletUiState.observe(viewLifecycleOwner,{
-            when(it){
+        viewModel.walletUiState.observe(viewLifecycleOwner, {
+            when (it) {
                 is WalletUiState.Loading -> showProgress()
                 is WalletUiState.Error -> showError(it.message)
                 is WalletUiState.Success -> showPlans(it.plans)
@@ -37,15 +46,30 @@ class WalletFragment : Fragment() {
     }
 
     private fun showPlans(plans: List<LoyaltyPlan>) {
-        Log.d("Wallet",plans.size.toString())
+        walletAdapter.setData(plans)
+        Log.d("Wallet", plans.size.toString())
     }
 
     private fun showError(message: String?) {
-        Log.d("Wallet",message!!)
+        Log.d("Wallet", message!!)
 
     }
 
     private fun showProgress() {
 
+    }
+
+    private fun setupRecyclerView() {
+        with(binding) {
+            rvPlansList.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = walletAdapter
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
