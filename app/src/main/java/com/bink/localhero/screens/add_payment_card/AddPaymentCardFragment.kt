b07d6177eabee11ade54f6634fd5c94d3b9b7d46
Joshua Best <Jbest@bink.com>
2022-03-09 16:44:34 +0000

    Payment card UI done

--- app/src/main/java/com/bink/localhero/screens/add_payment_card/AddPaymentCardFragment.kt
+++ app/src/main/java/com/bink/localhero/screens/add_payment_card/AddPaymentCardFragment.kt
@@ -1,8 +1,11 @@
 package com.bink.localhero.screens.add_payment_card
 
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.bink.localhero.base.BaseFragment
 import com.bink.localhero.databinding.FragmentAddPaymentCardBinding
+import com.bink.localhero.utils.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddPaymentCardFragment :
@@ -14,6 +17,46 @@ class AddPaymentCardFragment :
     override val viewModel: AddPaymentCardViewModel by viewModel()
 
     override fun setup() {
+        binding.etCardNumber.addTextChangedListener {
+            it?.trim().toString().let { cardNumber ->
+                cardNumber.presentedCardType().apply {
+                    binding.tvPaymentCardType.text = this.name
+                }
 
+                if (cardNumber.cardValidation() == PaymentCardType.NONE) {
+                    binding.tilCardNumber.error = "Invalid card number"
+                } else {
+                    binding.tilCardNumber.error = null
+                }
+            }
+
+            checkAddBtnEnable()
+        }
+
+        binding.etExpiry.addTextChangedListener {
+            binding.tilExpiry.error = cardExpiryErrorCheck(it?.trim().toString())
+            checkAddBtnEnable()
+        }
+
+        binding.btnAddPaymentCard.setOnClickListener {
+            Toast.makeText(requireContext(), "Post Card to Spreedly", Toast.LENGTH_LONG).show()
+        }
     }
+
+    private fun cardExpiryErrorCheck(text: String): String? {
+        with(text) {
+            if (!dateValidation()) {
+                return "Invalid expiry date"
+            }
+        }
+        return null
+    }
+
+    private fun checkAddBtnEnable() {
+        binding.btnAddPaymentCard.isEnabled =
+            ((binding.tilCardNumber.error == null && !binding.etCardNumber.text.isNullOrEmpty())
+                    && (binding.tilExpiry.error == null && !binding.etExpiry.text.isNullOrEmpty()))
+    }
+
+
 }
