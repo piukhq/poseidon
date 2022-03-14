32a7d4d5ffbbbd9500f9730740238296c63d1406
Joshua Best <Jbest@bink.com>
2022-03-14 10:22:30 +0000

    Created spreedy module

--- app/src/main/java/com/bink/localhero/base/MainApplication.kt
+++ app/src/main/java/com/bink/localhero/base/MainApplication.kt
@@ -3,18 +3,19 @@ package com.bink.localhero.base
 import android.app.Application
 import com.bink.localhero.di.networkModule
 import com.bink.localhero.di.roomModules
+import com.bink.localhero.di.spreedlyModule
 import com.bink.localhero.di.viewModelModule
 import com.bink.localhero.utils.LocalStoreUtils
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MainApplication:Application() {
+class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         LocalStoreUtils.createEncryptedPrefs(applicationContext)
         startKoin {
             androidContext(this@MainApplication)
-            modules(roomModules,networkModule, viewModelModule)
+            modules(roomModules, networkModule, viewModelModule, spreedlyModule)
         }
     }
 }
--- app/src/main/java/com/bink/localhero/di/SpreedlyModule.kt
+++ app/src/main/java/com/bink/localhero/di/SpreedlyModule.kt
@@ -1,38 +1,29 @@
 package com.bink.localhero.di
 
-import com.bink.localhero.data.remote.ApiService
 import com.bink.localhero.data.remote.SpreedlyService
-import com.bink.localhero.utils.*
+import com.bink.localhero.utils.BASE_URL
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 
-val networkModule = module {
-    single { provideDefaultOkHttpClient() }
-    single { provideRetrofit(get()) }
-    single { provideApiService(get()) }
+val spreedlyModule = module {
+    single { provideSpreedlyOkHttpClient() }
+    single { provideSpreedlyRetrofit(get()) }
+    single { provideSpreedlyService(get()) }
 }
 
-fun provideDefaultOkHttpClient(): OkHttpClient {
+fun provideSpreedlyOkHttpClient(): OkHttpClient {
     val interceptor = HttpLoggingInterceptor()
     interceptor.level = HttpLoggingInterceptor.Level.BODY
 
     val headerAuthorizationInterceptor = Interceptor { chain ->
         val request = chain.request().url().newBuilder().build()
 
-        val jwtToken =
-            LocalStoreUtils.getAppSharedPref(
-                LocalStoreUtils.KEY_TOKEN
-            )?.replace("\n", "")?.trim()
-
         val newRequest = chain.request().newBuilder()
             .header("Content-Type", "application/json")
-            .header("Authorization", " Bearer $jwtToken")
             .url(request)
             .build()
         val response = chain.proceed(newRequest)
@@ -44,7 +35,7 @@ fun provideDefaultOkHttpClient(): OkHttpClient {
         .addInterceptor(interceptor).build()
 }
 
-fun provideRetrofit(client: OkHttpClient): Retrofit {
+fun provideSpreedlyRetrofit(client: OkHttpClient): Retrofit {
     val retrofitBuilder = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
@@ -53,4 +44,5 @@ fun provideRetrofit(client: OkHttpClient): Retrofit {
     return retrofitBuilder.build()
 }
 
-fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+fun provideSpreedlyService(retrofit: Retrofit): SpreedlyService =
+    retrofit.create(SpreedlyService::class.java)
