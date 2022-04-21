3595e102a4c36b1e57f9b89753174bf9711fe5e6
Enoch Hankombo <ehankombo@bink.com>
2022-04-21 16:37:44 +0100

    Added edit text and logic for token

--- app/src/main/java/com/bink/localhero/screens/login/LoginFragment.kt
+++ app/src/main/java/com/bink/localhero/screens/login/LoginFragment.kt
@@ -1,9 +1,12 @@
 package com.bink.localhero.screens.login
 
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.core.view.isEmpty
 import androidx.navigation.fragment.findNavController
 import com.bink.localhero.base.BaseFragment
 import com.bink.localhero.databinding.FragmentLoginBinding
+import com.bink.localhero.utils.LocalStoreUtils
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginFragment : BaseFragment<LoginViewModel, FragmentLoginBinding>() {
@@ -15,6 +18,23 @@ class LoginFragment : BaseFragment<LoginViewModel, FragmentLoginBinding>() {
     override fun setup() {
         binding.btnGoToLogin.setOnClickListener {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToScanBarcodeFragment())
+        }
+
+        binding.btnProcessToken.setOnClickListener {
+            if (binding.etTokenValue.text.isNullOrEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Enter a token before proceding",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                LocalStoreUtils.setAppSharedPref(
+                    LocalStoreUtils.KEY_TOKEN,
+                    binding.etTokenValue.text.toString()
+                )
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToScanBarcodeFragment())
+
+            }
         }
     }
 
