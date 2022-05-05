4e1a4fdc04a551ce8ceaec144a1fc9a8f96a43e1
Joshua Best <Jbest@bink.com>
2022-05-05 17:01:04 +0100

    Map displaying with test data

--- app/src/main/java/com/bink/localhero/screens/map/MapCompose.kt
+++ app/src/main/java/com/bink/localhero/screens/map/MapCompose.kt
@@ -1,9 +1,22 @@
 package com.bink.localhero.screens.map
 
 import android.view.LayoutInflater
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.tooling.preview.Preview
 import com.bink.localhero.base.BaseFragment
 import com.bink.localhero.databinding.FragmentMapComposeBinding
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MapCompose : BaseFragment<MapViewModel, FragmentMapComposeBinding>() {
@@ -21,6 +34,28 @@ class MapCompose : BaseFragment<MapViewModel, FragmentMapComposeBinding>() {
 
     @Composable
     fun mapView() {
-
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = singapore),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
     }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        Surface(color = MaterialTheme.colors.background) {
+            mapView()
+        }
+    }
+
 }
