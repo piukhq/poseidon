8a903c19c8115f05cd7aca49a40e1f83f0cf9eb3
Enoch Hankombo <ehankombo@bink.com>
2022-05-20 14:12:55 +0300

    Added base api's

--- app/src/main/java/com/bink/localhero/screens/map/MapFragment.kt
+++ app/src/main/java/com/bink/localhero/screens/map/MapFragment.kt
@@ -2,9 +2,13 @@ package com.bink.localhero.screens.map
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.net.Uri
+import android.os.Looper
 import android.provider.Settings
 import android.view.LayoutInflater
 import androidx.activity.result.contract.ActivityResultContracts
@@ -18,9 +22,8 @@ import com.bink.localhero.model.bakery.Bakeries
 import com.bink.localhero.model.bakery.Properties
 import com.bink.localhero.utils.MapUiState
 import com.bink.localhero.utils.showDialog
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -29,15 +32,19 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Task
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-@SuppressLint("MissingPermission")
 class MapFragment : BaseFragment<MapViewModel, FragmentMapBinding>(), OnMapReadyCallback,
     GoogleMap.OnMarkerClickListener {
 
     private var fusedLocationProvider: FusedLocationProviderClient? = null
     private lateinit var map: GoogleMap
+    private lateinit var locationRequest: LocationRequest
+    companion object{
+        private const val REQUEST_CHECK_SETTINGS = 100
+    }
 
     override val bindingInflater: (LayoutInflater) -> FragmentMapBinding
         get() = FragmentMapBinding::inflate
@@ -73,11 +80,54 @@ class MapFragment : BaseFragment<MapViewModel, FragmentMapBinding>(), OnMapReady
         }
     }
 
+    private val locationCallback : LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+        }
+
+        override fun onLocationAvailability(p0: LocationAvailability) {
+            super.onLocationAvailability(p0)
+        }
+    }
+
     override fun setup() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
         fusedLocationProvider = LocationServices.getFusedLocationProviderClient(requireContext())
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(requireContext())
+
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(requireActivity(),
+                        REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
         checkLocationPermission()
     }
 
@@ -141,6 +191,7 @@ class MapFragment : BaseFragment<MapViewModel, FragmentMapBinding>(), OnMapReady
     }
 
     private fun getLocation() {
+        fusedLocationProvider?.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
         fusedLocationProvider?.getCurrentLocation(
             LocationRequest.PRIORITY_HIGH_ACCURACY,
             CancellationTokenSource().token
@@ -178,4 +229,12 @@ class MapFragment : BaseFragment<MapViewModel, FragmentMapBinding>(), OnMapReady
         ) == PackageManager.PERMISSION_GRANTED
 
 
+    private fun isGpsTurnedOn(): Boolean {
+        // Calling Location Manager
+        val mLocationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Checking GPS is enabled
+        return mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
 }
